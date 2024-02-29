a496af4e3bf673301ee400b0e62d85223a0eaca4
osman-butt <butt.osman@gmail.com>
2024-02-29 16:29:12 +0100

    added authProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import AuthProvider from "./security/AuthProvider";
 
-//Observe the "!"" below, which tells TypeScript that you are certain document.getElementById("root") 
+//Observe the "!"" below, which tells TypeScript that you are certain document.getElementById("root")
 //will not return null,
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
