be5a979388c34ed9e12b265c12f77a4ecff9b140
osman-butt <butt.osman@gmail.com>
2024-02-29 13:39:14 +0100

    category query params

--- src/recipes/Categories.tsx
+++ src/recipes/Categories.tsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import { getCategories } from "../services/apiFacade";
+import { Link } from "react-router-dom";
 
 export const Categories = () => {
   const [categories, setCategories] = useState<Array<string>>();
   useEffect(() => {
-    getCategories().then((res) => setCategories(res));
+    getCategories().then(res => setCategories(res));
   }, []);
   return (
     <>
@@ -12,10 +13,10 @@ export const Categories = () => {
       <p>Browse recipes by category.</p>
 
       <ul>
-        {categories?.map((category) => (
+        {categories?.map(category => (
           <li key={category}>
-            {category}
-            {/* <Link to={`/recipes?category=${category}`}>{category}</Link> */}
+            {/* {category} */}
+            <Link to={`/recipes?category=${category}`}>{category}</Link>
           </li>
         ))}
       </ul>
